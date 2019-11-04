186658c473aaab44b5aa1761c5a60dbefea18cbe
Svyatoslav Savilov <50026241+snegmen@users.noreply.github.com>
2019-11-04 19:30:28 +0700

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -5,20 +5,16 @@ const User = require('../models/user');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
-module.exports.createUser = (req, res, next) => {
+module.exports.createUser = (req, res) => {
   if (Object.keys(req.body).length === 0) return res.status(400).send({ message: 'Тело запроса пустое' });
   const {
     name, about, avatar, email, password,
   } = req.body;
-
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     })
       .then((user) => {
-        if (!user) {
-          throw new RequestError('Что-то не так с данными пользователя');
-        }
         res.status(201).send({
           _id: user._id,
           email: user.email,
@@ -27,7 +23,7 @@ module.exports.createUser = (req, res, next) => {
           avatar: user.avatar,
         });
       })
-      .catch(next));
+      .catch(() => res.status(400).send({ message: 'Произошла ошибка при создании пользователя' })));
 };
 
 module.exports.login = (req, res) => {
