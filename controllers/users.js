60963ddfa82b15d16eb28ec40447cbe46b237495
Svyatoslav Savilov <50026241+snegmen@users.noreply.github.com>
2019-11-08 11:30:09 +0700

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -4,8 +4,6 @@ const User = require('../models/user');
 const Error500 = require('../errors/500-err');
 const NotFoundError = require('../errors/not-found-err');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 // eslint-disable-next-line consistent-return
 module.exports.createUser = (req, res, next) => {
   if (Object.keys(req.body).length === 0) return res.status(400).send({ message: 'Тело запроса пустое' });
@@ -30,7 +28,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.status(201).cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
