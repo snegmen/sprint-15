dc7f4c8f0838e30fcf8a18922b622d8c01648f7f
Svyatoslav Savilov <50026241+snegmen@users.noreply.github.com>
2019-11-08 11:19:34 +0700

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret');
   } catch (err) {
     return res
       .status(401)
